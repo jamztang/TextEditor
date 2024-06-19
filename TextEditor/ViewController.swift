64829e2e91942e2f5e841fb3386463ffa9789156
James Tang <james@goodnotesapp.com>
2024-06-19 18:55:17 +0800

    Add reproduce button

--- TextEditor/ViewController.swift
+++ TextEditor/ViewController.swift
@@ -7,25 +7,88 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Issue {
+    enum Content {
+        case html(String)
+    }
 
-    @IBOutlet weak var textView: UITextView!
+    let title: String
+    let content: Content
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        let html = """
-        <ol style="color: white">
+    // Mock Data
+    static let typex1501: Issue = .init(
+        title: "TYPEX-1501",
+        content: .html("<span style='color: green'><b>Ipsum</b> world Ipsum world</span>")
+    )
+    static let typex1493: Issue = .init(
+        title: "TYPEX-1493",
+        content: .html("""
+        <span style="color: green">
+        <p style="text-indent: 0px;">ipsum world</p>
+        <p style="text-indent: 15px;">world</p>
+        <p style="text-indent: 30px;">world ipsum</p>
+        <p style="text-indent: 45px;">world</p>
+        </span>
+        """)
+    )
+    static let typex1698: Issue = .init(
+        title: "TYPEX-1501",
+        content: .html("""
+        <ol style="color: green">
             <li>one</li>
                 <ol>
                     <li>two</li>
                 </ol>
         </ol>
-        """
+        """)
+    )
+}
 
-        let data = Data(html.utf8)
-        if let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
-            textView.attributedText = attributedString
+extension Issue {
+    func action(_ handler: @escaping UIActionHandler) -> UIAction {
+        .init(title: title, handler: handler)
+    }
+}
+
+extension Issue.Content {
+    var attributedString: NSAttributedString? {
+        switch self {
+        case .html(let string):
+            let data = Data(string.utf8)
+            guard let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) else {
+                return nil
+            }
+            return attributedString
         }
+    }
+}
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var resetMenu: UIMenu!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let issues = [
+            Issue.typex1501,
+            .typex1493,
+            .typex1698
+        ]
+
+        let actions = issues.map { issue in
+            return issue.action { [weak self] action in
+                self?.handle(issue)
+            }
+        }
+
+        resetButton.menu = UIMenu(title: "", children: actions)
+
+    }
+
+    func handle(_ issue: Issue) {
+        textView.attributedText = issue.content.attributedString
     }
 }
